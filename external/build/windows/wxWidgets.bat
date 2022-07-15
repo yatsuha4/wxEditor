02bc31a998da9f501cc1b6b88fc9e57a72467b36
Yasui Kentarow <yasuikentarow@gmail.com>
2022-07-16 06:47:14 +0900

    wxWidgets-3.2ではnmakeが通らないので削除

--- external/build/windows/wxWidgets.bat
+++
@@ -1,9 +0,0 @@
-set CURDIR=%~dp0
-set SRCDIR=%CURDIR%..\..\wxWidgets
-
-pushd "%SRCDIR%\build\msw"
-nmake /a /f makefile.vc TARGET_CPU=X64 clean
-nmake /a /f makefile.vc TARGET_CPU=X64 BUILD=release clean
-nmake /a /f makefile.vc TARGET_CPU=X64
-nmake /a /f makefile.vc TARGET_CPU=X64 BUILD=release
-popd
