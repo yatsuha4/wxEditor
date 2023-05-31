70dd0d54ec29a957b746e3f2d04e0f5ae6f1b21b
Yasui Kentarow <yasuikentarow@gmail.com>
2023-06-01 01:01:34 +0900

    VS2019でビルドするバッチファイルを追加

---
+++ external/build/vs2019/build.bat
@@ -0,0 +1,2 @@
+MSBuild ..\..\wxWidgets\build\msw\wx_vc16.sln /t:Build /p:Configuration=Release;Platform=x64
+MSBuild ..\..\wxWidgets\build\msw\wx_vc16.sln /t:Build /p:Configuration=Debug;Platform=x64
