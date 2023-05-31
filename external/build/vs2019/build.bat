ef9e6738c143149cd72ece57fd682e82cf726958
Yasui Kentarow <yasuikentarow@gmail.com>
2023-06-01 01:30:12 +0900

    相対パスを正しく取得

--- external/build/vs2019/build.bat
+++ external/build/vs2019/build.bat
@@ -1,2 +1,2 @@
-MSBuild ..\..\wxWidgets\build\msw\wx_vc16.sln /t:Build /p:Configuration=Release;Platform=x64
-MSBuild ..\..\wxWidgets\build\msw\wx_vc16.sln /t:Build /p:Configuration=Debug;Platform=x64
+set CURDIR=%~dp0
+MSBuild "%CURDIR%..\..\wxWidgets\build\msw\wx_vc16.sln" /t:Build /p:Configuration=Release;Platform=x64
